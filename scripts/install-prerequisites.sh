fda35fbc712b2c0d54d5eeca214e97ca820cbd09
lhdjply <lhdjply@126.com>
2024-06-12 08:34:40 +0800

    32bit build use ubuntu22.04
    Signed-off-by: lhdjply <lhdjply@126.com>

--- scripts/install-prerequisites.sh
+++ scripts/install-prerequisites.sh
@@ -5,6 +5,8 @@
 # here, such as MicroPython and PC simulator packages.
 #
 # Note: This script is run by the CI workflows.
+UBUNTU_VERSION=$(lsb_release -sr)
+
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install gcc gcc-multilib g++-multilib ninja-build \
@@ -12,5 +14,7 @@ libpng-dev libjpeg-turbo8-dev libfreetype6-dev \
 libglew-dev libglfw3-dev \
 libpng-dev:i386 libjpeg-dev:i386 libfreetype6-dev:i386 \
 ruby-full gcovr cmake  python3 pngquant libinput-dev libxkbcommon-dev libdrm-dev pkg-config
-sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED /usr/lib/python3.12/EXTERNALLY-MANAGED.BAK
+if [ "$UBUNTU_VERSION"=="24.04" ]; then
+    sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED /usr/lib/python3.12/EXTERNALLY-MANAGED.BAK
+fi
 pip3 install pypng lz4
